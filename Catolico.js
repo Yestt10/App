5bfaadb15f1c612fe3d565e144f7b35fd0f9cee2
Yestt10 <147564943+Yestt10@users.noreply.github.com>
2023-10-31 20:03:10 -0300

    Update Catolico.js

--- Catolico.js
+++ Catolico.js
@@ -1,4 +1,4 @@
- // Dados de exemplo com títulos, URLs de imagens e URLs de links
+   // Dados de exemplo com títulos, URLs de imagens e URLs de links
         const feedData = [
             {
                 title: 'Título do Artigo 1',
@@ -12,31 +12,6 @@
             },
             // Adicione mais itens conforme necessário
         ];
-
-        // Função para exibir os itens do feed na página
-        function renderFeed() {
-            const feedContainer = document.getElementById('feed');
-
-            feedData.forEach(item => {
-                const feedItem = document.createElement('div');
-                feedItem.classList.add('feed-item');
-
-                const image = document.createElement('img');
-                image.src = item.imageUrl;
-                image.alt = 'Imagem do Artigo';
-
-                const title = document.createElement('h2');
-                const link = document.createElement('a');
-                link.href = item.linkUrl;
-                link.innerText = item.title;
-
-                title.appendChild(link);
-
-                feedItem.appendChild(image);
-                feedItem.appendChild(title);
-                feedContainer.appendChild(feedItem);
-            });
-        }
 
         // Chama a função para renderizar o feed quando a página carrega
         window.onload = renderFeed;
