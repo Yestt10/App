1f4b0eb056e37eade13e82983781fd1bb6e3b8cf
Yestt10 <147564943+Yestt10@users.noreply.github.com>
2023-11-01 13:39:38 -0300

    Update Home-Alcir.js

--- Home-Alcir.js
+++ Home-Alcir.js
@@ -14,83 +14,24 @@
        {
                 title: 'Título do Artigo 2',
                 imageUrl: 'https://via.placeholder.com/100',
+                linkUrl: 'https://www.alcir.net/2023/10/cicatrizes-as-facadas-invisiveis.html'
+            },
+       {
+                title: 'Cicatrizes - Facadas Invisíveis que Levamos',
+                imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgBpf21soDrCC7H_eRsCi8wUroSzjy7vjK9ckZTOmu-lwRKWm3eG5CZGBDKAgaF9wRTIcwOyneDr6QrWriRWNc9KJxsKEPpwmx9jSS2VnTMe5PwVnPfQcvAcrvI_KTa4RmVK8xGBNl3ShdoGVSDKAHUI2YYelz7tKWAEvNAowHLlo6kCosu6bqGh968ax1w/w554-h311-c-rw/faca-com-sangue.jpg',
                 linkUrl: 'https://www.google.com'
             },
        {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
+                title: 'Encontrando Luz nas Sombras - Anda Desanimado(a)? Leia esse Texto',
+                imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjMk2bRycyRZMasSlAJx2eJxiYmCbCE7nYcK5ABjZocqtVv145Ano1r23ror2Is1DedBC_znhYlnDWRvHJIzX5h8ymQ-CTUIL6YnIZwqwrb2O6UHv7JqyB8O3XJs3L6iiWgrB8owyVDsAMFSO4xntHOwtNol9WQq0riVBR7kbBtmMUiK864Ugg5PM0G_HWA/w554-h311-c-rw/luz-nas-sobra.jpg',
+                linkUrl: 'https://www.alcir.net/2023/10/encontrando-luz-nas-sombras.html'
             },
        {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
+                title: 'Apressando-se Devagar - No Ritmo do Tempo',
+                imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEho1fL_buEy6Mf1joudXOKG2qLJYqSndIqVlJb7eLnwMil7LXPltaMpFGfjf2Miq5VO4G3OXGjTncWziPR-b6d7A7-0d9ExnmZopUN1et9pQzYG4uDT_cgaRTDnvl6bmwWaWTNZvs__dTHftHVz29jCDMF-AqxlOKU7CVyespfyMPhwFkg3zh8fDTj5y5__/s320-rw/tempo.jpg',
+                linkUrl: 'https://www.alcir.net/2023/10/apressando-se-devagar-no-ritmo-do-tempo.html'
             },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
-       {
-                title: 'Título do Artigo 2',
-                imageUrl: 'https://via.placeholder.com/100',
-                linkUrl: 'https://www.google.com'
-            },
+                
         ];
 
         // Função para exibir os itens do feed na página
