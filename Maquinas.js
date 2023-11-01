a18161b1584cdc3604156cdf73b2d7b00c904b2b
Yestt10 <147564943+Yestt10@users.noreply.github.com>
2023-11-01 17:29:17 -0300

    Update Maquinas.js

--- Maquinas.js
+++ Maquinas.js
@@ -17,19 +17,24 @@
                 linkUrl: 'https://mpago.li/2KbgYNf'
             },
        {
-                title: 'Cicatrizes - Facadas Invisíveis que Levamos',
-                imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgBpf21soDrCC7H_eRsCi8wUroSzjy7vjK9ckZTOmu-lwRKWm3eG5CZGBDKAgaF9wRTIcwOyneDr6QrWriRWNc9KJxsKEPpwmx9jSS2VnTMe5PwVnPfQcvAcrvI_KTa4RmVK8xGBNl3ShdoGVSDKAHUI2YYelz7tKWAEvNAowHLlo6kCosu6bqGh968ax1w/w554-h311-c-rw/faca-com-sangue.jpg',
-                linkUrl: 'https://www.alcir.net/2023/10/cicatrizes-as-facadas-invisiveis.html'
+                title: 'Ton T1',
+                imageUrl: 'https://i.imgur.com/q7rUsXH.jpg',
+                linkUrl: 'https://www.ton.com.br/catalogo/?referrer=97B2DEDE-8D48-4FA6-848E-ADE657DEC214&utm_medium=invite_share&utm_source=revendedor'
             },
        {
-                title: 'Encontrando Luz nas Sombras - Anda Desanimado(a)? Leia esse Texto',
-                imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjMk2bRycyRZMasSlAJx2eJxiYmCbCE7nYcK5ABjZocqtVv145Ano1r23ror2Is1DedBC_znhYlnDWRvHJIzX5h8ymQ-CTUIL6YnIZwqwrb2O6UHv7JqyB8O3XJs3L6iiWgrB8owyVDsAMFSO4xntHOwtNol9WQq0riVBR7kbBtmMUiK864Ugg5PM0G_HWA/w554-h311-c-rw/luz-nas-sobra.jpg',
-                linkUrl: 'https://www.alcir.net/2023/10/encontrando-luz-nas-sombras.html'
+                title: 'Ton T1 Chip',
+                imageUrl: 'https://i.imgur.com/vMUSUGC.jpg',
+                linkUrl: 'https://www.ton.com.br/catalogo/?referrer=97B2DEDE-8D48-4FA6-848E-ADE657DEC214&utm_medium=invite_share&utm_source=revendedor'
             },
        {
-                title: 'Apressando-se Devagar - No Ritmo do Tempo',
-                imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEho1fL_buEy6Mf1joudXOKG2qLJYqSndIqVlJb7eLnwMil7LXPltaMpFGfjf2Miq5VO4G3OXGjTncWziPR-b6d7A7-0d9ExnmZopUN1et9pQzYG4uDT_cgaRTDnvl6bmwWaWTNZvs__dTHftHVz29jCDMF-AqxlOKU7CVyespfyMPhwFkg3zh8fDTj5y5__/s320-rw/tempo.jpg',
-                linkUrl: 'https://www.alcir.net/2023/10/apressando-se-devagar-no-ritmo-do-tempo.html'
+                title: 'Ton T2',
+                imageUrl: 'https://i.imgur.com/M5oOb03.png',
+                linkUrl: 'https://www.ton.com.br/catalogo/?referrer=97B2DEDE-8D48-4FA6-848E-ADE657DEC214&utm_medium=invite_share&utm_source=revendedor'
+            },
+                       {
+                title: 'Ton T3',
+                imageUrl: 'https://i.imgur.com/c7ZXrMl.png',
+                linkUrl: 'https://www.ton.com.br/catalogo/?referrer=97B2DEDE-8D48-4FA6-848E-ADE657DEC214&utm_medium=invite_share&utm_source=revendedor'
             },
                 
         ];
